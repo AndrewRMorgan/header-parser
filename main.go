c2fcdc7082fbbada88813d09be69c7ee69359a0f
AndrewRMorgan <andrew.r.morgan@gmail.com>
2017-04-12 10:41:17 +1000

    create code to parse ip address, and split language list and user agent fields

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"net"
+	"strings"
 )
 
 func main() {
@@ -17,10 +19,29 @@ func main() {
 }
 
 func Router(w http.ResponseWriter, r *http.Request) {
-	ip := r.RemoteAddr
-	lang := r.Header.Get("Accept-Language")
-	sys := r.Header.Get("User-Agent")
-	fmt.Println(ip)
+	ip, _, err := net.SplitHostPort(r.RemoteAddr)
+	if err != nil {
+		fmt.Println("SplitHostPort returned an error")
+		return
+	}
+	userIp := net.ParseIP(ip)
+	if userIp == nil {
+		fmt.Println("ParseIP returned nil")
+	}
+
+	langList := r.Header.Get("Accept-Language")
+	result := strings.Split(langList, ",")
+	lang := result[0]
+
+	f := func(c rune) bool {
+		return c == '(' || c == ')'
+	}
+	userAgent := r.Header.Get("User-Agent")
+	fields := strings.FieldsFunc(userAgent, f)
+
+	sys := fields[1]
+
+	fmt.Println(userIp)
 	fmt.Println(lang)
 	fmt.Println(sys)
 }
